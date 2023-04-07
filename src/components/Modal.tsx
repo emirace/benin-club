9e5bab397ddeca4c214f2ee992bbc30564a6ebe5
emirace <emmanuelakwuba57@gmail.com>
2023-04-07 12:40:47 +0100

    hi

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,14 +12,15 @@ const Modal: FC<Props> = ({ isOpen, onClose, children }) => {
   }
 
   return (
-    <div className="fixed inset-0 z-20 overflow-y-auto">
+    <div className="fixed inset-0 z-40 overflow-y-auto">
       <div className="flex items-center justify-center min-h-screen">
         <div
           className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75"
           onClick={onClose}
         />
-        <div className="bg-white rounded-lg shadow-lg relative z-10">
-          <div className="absolute top-0 right-0 p-2">
+        <div className="bg-white rounded-lg shadow-lg relative z-30">
+          <div className="h-24 w-full" />
+          <div className="absolute top-24 md:top-0 right-0 p-2">
             <button
               className="text-gray-400 hover:text-gray-500 focus:outline-none"
               onClick={onClose}
