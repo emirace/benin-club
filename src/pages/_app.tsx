532015c3c6c673b5c0823e3a1429ab66c79ed21b
emirace <emmanuelakwuba57@gmail.com>
2023-04-07 12:22:37 +0100

    download

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navbar from '@/components/Navbar';
+import FooterSection from '@/sections/FooterSection';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+      <FooterSection />
+    </div>
+  );
 }
