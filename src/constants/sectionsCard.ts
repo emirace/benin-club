e8ee38d371f8049818e777e20f15e0bb3a374bf6
emirace <emmanuelakwuba57@gmail.com>
2023-04-08 19:34:15 +0100

    image

--- src/constants/sectionsCard.ts
+++ src/constants/sectionsCard.ts
@@ -1,12 +1,12 @@
 export const cards = [
   {
-    imageUrl: '/images/billiards/image1.jpg',
+    imageUrl: '/images/billiards/image1.JPG',
     title: 'Billiards & Snooker',
     info: 'With three full size tables Benin Club Snookers/Billiard Section is one of the oldest section in the club. It is a socially vibrant place in the club as tournament and parties are frequently held.',
     link: '/billiards',
   },
   {
-    imageUrl: '/images/busStop/image1.jpg',
+    imageUrl: '/images/busStop/image1.JPG',
     title: 'Bus Stop',
     info: 'This section that can best be referred to as the parliament of Benin club with a very robust membership is a very comfortable place to seat. The section over the year has provided socially vibrant space for members of the club to learn. The section organizes the manifesto night of the club every year. You may not have satisfied your yearning for joining the club if you have not made a stop at the section. The section enhance fellowship and interaction amongst members, Membership cuts across all segments of the society.',
     link: '/busStop',
@@ -18,7 +18,7 @@ export const cards = [
     link: '/darts',
   },
   {
-    imageUrl: '/images/golf/image1.jpg',
+    imageUrl: '/images/golf/image1.JPG',
     title: 'Golf',
     info: 'Undoubtedly the golf game is for the Noble Mind one can therefore say with all modesty that the Golf section has the most active members who take playing their game seriously like one preparing for the Combridge Exam. Playing golf in Benin Club is always smooth because of the well Maintained Course. It is a section to be for all round fun and social life, if in doubt just try hole 19 to know what fun and social life is',
     link: '/golf',
@@ -30,25 +30,25 @@ export const cards = [
     link: '/lawntennis',
   },
   {
-    imageUrl: '/images/leisure/image1.jpg',
+    imageUrl: '/images/leisure/image1.JPG',
     title: 'Leisure Games',
     info: 'Aka as Rock the Section is Commissioned for indoor games such as Scrabble, Chess Ayo, Whot, Monopoly, Draught, Ludo E.t.c with a dedicated membership of young vibrant clubers who are out to see to the growth of its member physically, morally and socially, leisure section also know as Aso Rock is the place for any forward looking member of the club to be.',
     link: '/leisuregames',
   },
   {
-    imageUrl: '/images/squash/image1.jpg',
+    imageUrl: '/images/squash/image1.JPG',
     title: 'Squash',
     info: 'With the courts and a terrace that can comfortably sit about 80 people and with a soon to be ready Message/Gym Room, the section is the place to be. If truly wish to know if you are fit, try squash. The section unarguably is one of the social hub in the club.',
     link: '/squash',
   },
   {
-    imageUrl: '/images/swimming/image1.jpg',
+    imageUrl: '/images/swimming/image1.JPG',
     title: 'Swimming Pool',
     info: 'With a standard swimming pool, the section has created room for mony members to learn swimming which is. It is worthy to mention that his Excellency Mr. Godwin Nogheghase Obaseki learnt to swim here as a young boy years ago. It is a socially active place to be',
     link: '/swimmingpool',
   },
   {
-    imageUrl: '/images/tableTennis/image1.jpg',
+    imageUrl: '/images/tableTennis/image1.JPG',
     title: 'Table Tennis',
     info: 'With a fully air conditioned lounge well floored for playing and standard playing facilities and a taste fully furnished cool room for social and quiet moment undoubtedly it is a place to be.',
     link: '/tabletennis',
