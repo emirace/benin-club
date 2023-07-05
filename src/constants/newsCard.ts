7f6386adff1b82daa17f203083a9744a15fdb25d
emirace <emmanuelakwuba57@gmail.com>
2023-07-05 13:23:10 +0100

    hi

--- src/constants/newsCard.ts
+++ src/constants/newsCard.ts
@@ -3,18 +3,20 @@ import { News } from '@/types/newsCard';
 export const news: News[] = [
   {
     _id: 1,
-    image: '/images/image2.jpg',
+    images: ['/images/image2.jpg'],
     date: 'March 28, 2023',
     tags: ['Tag1', 'Tag2'],
     title: 'News Title 1',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    slug: '',
   },
   {
     _id: 2,
-    image: '/images/image3.jpg',
+    images: ['/images/image3.jpg'],
     date: 'March 28, 2023',
     tags: ['Tag1', 'Tag2'],
     title: 'News Title 1',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    slug: '',
   },
 ];
